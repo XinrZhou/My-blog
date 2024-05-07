c137f4150e52875cb67b51ef3f689e303d8e73bf
XinrZhou <1352492197@qq.com>
2024-05-07 09:36:16 +0800

    feat: update github actions file

--- vite.config.ts
+++ vite.config.ts
@@ -15,8 +15,8 @@ export default defineConfig({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8855/api',
-        // target: 'http://124.223.175.150:8855/api',
+        // target: 'http://localhost:8855/api',
+        target: 'http://124.223.175.150:8855/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       },
